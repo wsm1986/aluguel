32145c30952a42f4cb65922e60f017c05977cc27
Wellington Moreira <dbaldani@uoldiveo.com>
2017-01-31 20:02:20 -0200

    conf banco[A

--- src/main/java/com/aluguel/conf/MyConf.java
+++ src/main/java/com/aluguel/conf/MyConf.java
@@ -4,6 +4,8 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class MyConf {
 
 }
